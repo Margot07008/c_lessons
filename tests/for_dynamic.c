0cbc0258dade9d626ef4e3a9832951fdc68e5c6e
margot07008 <9311margarita@gmail.com>
2020-03-23 16:21:46 +0300

    reload

--- tests/for_dynamic.c
+++ tests/for_dynamic.c
@@ -12,32 +12,37 @@ int test_fot_dynamic()
     global_struct.size = 5;
     int template[9] = {21,38,51,60,65,44,27,14,5};
     create_matrix_multy_test(0);
-    assert(is_equal(global_struct.arr_d, template, global_struct.size) == 1);
+    if(!is_equal(global_struct.arr_d, template, global_struct.size))
+        return (0);
     free_all ();
 
 
     global_struct.size = 10;
     int template1[19] = {91,173,246,310,365,411,448,476,495,505,414,332,259,195,140,94,57,29,10};
     create_matrix_multy_test(0);
-    assert(is_equal(global_struct.arr_d, template1, global_struct.size) == 1);
+    if (!is_equal(global_struct.arr_d, template1, global_struct.size))
+        return (0);
     free_all ();
 
     global_struct.size = 5;
     int template2[9] = {0,0,0,0,0,0,0,0};
     create_matrix_multy_test(1);
-    assert(is_equal(global_struct.arr_d, template2, global_struct.size) == 1);
+    if (!is_equal(global_struct.arr_d, template2, global_struct.size))
+        return (0);
     free_all ();
 
     global_struct.size = 10;
     int template3[19] = {-90,-171,-243,-306,-360,-405,-441,-468,-486,-495,-405,-324,-252,-189,-135,-90,-54,-27,-9};
     create_matrix_multy_test(2);
-    assert(is_equal(global_struct.arr_d, template3, global_struct.size) == 1);
+    if(!is_equal(global_struct.arr_d, template3, global_struct.size))
+        return 0;
     free_all ();
 
     global_struct.size = 1;
     int template4[1] = {0};
     create_matrix_multy_test(1);
-    assert(is_equal(global_struct.arr_d, template4, global_struct.size) == 1);
+    if (!is_equal(global_struct.arr_d, template4, global_struct.size))
+        return (0);
     free_all ();
 
     return(1);

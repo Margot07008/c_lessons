bedb47eae7edc938a839557802716db051a194df
margot07008 <9311margarita@gmail.com>
2020-03-23 15:36:03 +0300

    dfvdv

--- cmake-build-debug/CMakeFiles/dz2.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for dz2.
-# This may be replaced when dependencies are built.
--- tests/test.cpp
+++ tests/test.cpp
@@ -8,11 +8,10 @@ extern "C" {
 #include "../includes/header.h"
 }
 
-TEST(STATIC, unit_tests) {
-    ASSERT_EQ( test_for_static(), 1);
-}
+//TEST(STATIC, unit_tests) {
+//    ASSERT_EQ( test_for_static(), 1);
+//}
 
 TEST(DYNAMIC, unit_tests) {
     ASSERT_EQ( test_fot_dynamic(), 1);
-
 }
